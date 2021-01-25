8a8f256e7697a784d2d50ef366feb19db34cb00c
Evan Teems <evanteems40@yahoo.com>
2021-01-24 20:38:54 -0800

    index.html

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Daily Bootcamp Scheduler
+
+This planner allows the user to save any events that they choose to store for each and every work hour 
+
+This planner goes from 9 am to 5 pm. 
+
+Deployed Link: https://evanteems.github.io/Daily-Bootcamp-Scheduler/
+
+My Github: https://github.com/evanteems/Daily-Bootcamp-Scheduler
--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 $(window).on("load", function () {
  
-    // use moment to get the date/time in nice format
+    // Date & Time Formatted here
     var currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
     $("#DaysOTW").append(currentDate);
 
     var currentTime = moment().format("H");
     
-    // calling the fields by whole time, ie. 9, 10, 11 in order
-    // to make it easier to loop around it
-
-    // loop through all the times to determine if past, present, future
-    // use existing CSS to define what they are color coded by grey,red, green
     for (i = 9; i <= 18; i++) {
 
 
@@ -41,8 +36,8 @@ $(window).on("load", function () {
 
     $("button").on("click", function() {
 
-        //The entry is saved when the buttoon is clicked
-        //Fields all in local storage
+        //Button is clicked the entry will be saved
+
         
         for(i=9; i<= 17; i++) {
 
